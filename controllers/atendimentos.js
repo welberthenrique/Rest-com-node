b098970286c7f21b46eee626dfa479024b060fb5
welberthenrique <welberthenrique@gmail.com>
2022-04-18 00:39:46 -0300

    commit inicial projeto

---
+++ controllers/atendimentos.js
@@ -0,0 +1,14 @@
+const Atendimento = require("../models/atendimentos");
+
+module.exports = (app) => {
+  app.get("/atendimentos", (req, res) =>
+    res.send("Você está na rota de atendimentos e esta realizando um GET")
+  );
+
+  app.post("/atendimentos", (req, res) => {
+    const atendimento = req.body;
+
+    Atendimento.adiciona(atendimento);
+    res.send("POST atendimento");
+  });
+};
